dc280b77a35b87798c9a88d6ec37d7f03d9311d1
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2015-05-05 23:34:00 +0900

    [Implement] - fixed slide sidebar

--- site/public/js/directives.js
+++ site/public/js/directives.js
@@ -104,4 +104,55 @@ angular.module('myApp.directives', [])
         });
       }
     };
-  });
+  })
+  .directive("stickyNav", ['$window', function stickyNav($window){
+    function stickyNavLink(scope, element){
+      var w = angular.element($window),
+          size = element[0].clientHeight,
+          top = 0;
+
+      /*
+       * on scroll we just check the page offset
+       * if it's bigger than the target size we fix the controls
+       * otherwise we display them inline
+       */
+      function toggleStickyNav(){
+        if(!element.hasClass('slide__panel-affix') && $window.pageYOffset > top + size){
+          element.addClass('slide__panel-affix');
+        } else if(element.hasClass('slide__panel-affix') && $window.pageYOffset <= top + size){
+          element.removeClass('slide__panel-affix');
+        }
+      }
+
+      /*
+       * We update the top position -> this is for initial page load,
+       * while elements load
+       */
+      scope.$watch(function(){
+        return element[0].getBoundingClientRect().top + $window.pageYOffset;
+      }, function(newValue, oldValue){
+        if(newValue !== oldValue && !element.hasClass('slide__panel-affix')){
+          top = newValue;
+        }
+      });
+
+      /*
+       * Resizing the window displays the controls inline by default.
+       * This is needed to calculate the correct boundingClientRect.
+       * After the top is updated we toggle the nav, eventually
+       * fixing the controls again if needed.
+       */
+      w.bind('resize', function stickyNavResize(){
+        element.removeClass('slide__panel-affix');
+        top = element[0].getBoundingClientRect().top + $window.pageYOffset;
+        toggleStickyNav();
+      });
+      w.bind('scroll', toggleStickyNav);
+    }
+
+    return {
+      scope: {},
+      restrict: 'A',
+      link: stickyNavLink
+    };
+  }]);
